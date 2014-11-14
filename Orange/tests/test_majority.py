89ef4b1f91ddba3d1fe9c9a6262b46fe17ab9c11
Tomaz Curk <tomaz.curk@fri.uni-lj.si>
2014-11-14 10:32:02 +0100

    Minor.

--- Orange/tests/test_majority.py
+++ Orange/tests/test_majority.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 
 from Orange import data
-import Orange.classification.majority as majority_
+import Orange.classification.majority as majority
 
 
 class MajorityTest(unittest.TestCase):
@@ -13,7 +13,7 @@ class MajorityTest(unittest.TestCase):
         y = np.random.random_integers(1, 3, (nrows, 1)) // 2
         y[0] = 4
         t = data.Table(x, y)
-        learn = majority_.MajorityFitter()
+        learn = majority.MajorityFitter()
         clf = learn(t)
 
         x2 = np.random.random_integers(1, 3, (nrows, ncols))
@@ -28,7 +28,7 @@ class MajorityTest(unittest.TestCase):
         heavy = 3
         w = (y == heavy) * 123 + 1
         t = data.Table(x, y, W=w)
-        learn = majority_.MajorityFitter()
+        learn = majority.MajorityFitter()
         clf = learn(t)
 
         x2 = np.random.random_integers(1, 3, (nrows, ncols))
@@ -37,14 +37,14 @@ class MajorityTest(unittest.TestCase):
 
     def test_empty(self):
         iris = data.Table('iris')
-        learn = majority_.MajorityFitter()
+        learn = majority.MajorityFitter()
         clf = learn(iris[:0])
         y = clf(iris[0], clf.Probs)
         self.assertTrue(np.allclose(y, y.sum() / y.size))
 
     def test_missing(self):
         iris = data.Table('iris')
-        learn = majority_.MajorityFitter()
+        learn = majority.MajorityFitter()
         for e in iris[:len(iris)/2:2]:
             e.set_class("?")
         clf = learn(iris)
@@ -52,7 +52,7 @@ class MajorityTest(unittest.TestCase):
         self.assertTrue((y == 2).all())
 
         iris = data.Table('iris')
-        learn = majority_.MajorityFitter()
+        learn = majority.MajorityFitter()
         for e in iris:
             e.set_class("?")
         clf = learn(iris)
@@ -61,5 +61,5 @@ class MajorityTest(unittest.TestCase):
 
     def test_continuous(self):
         autompg = data.Table('auto-mpg')
-        learn = majority_.MajorityFitter()
+        learn = majority.MajorityFitter()
         self.assertRaises(ValueError, learn, autompg)
